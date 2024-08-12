6184d0cb7c5639c9bfb8dad532ebcbb1119e1789
tadaHrd <hrudka.tadeas@gmail.com>
2024-08-12 21:39:31 +0200

    Forgot docs for crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,52 @@
-//! TODO: docs
+//! An esoteric virtual machine.
+//!
+//! Create a new machine with [`Machine::new`] and load
+//! machine code and data to it with [`Machine::load`].
+//!
+//! # Examples
+//!
+//! ```rust
+//! use esoteric_vm::{esoteric_assembly, machine::Machine};
+//!
+//! # fn main() -> Machine {
+//! // initialize a new machine
+//! let mut machine = Machine::default();
+//!
+//! // assembly code for the machine
+//! let asm = esoteric_assembly! {
+//!     // initialize dot pointer so that IO operations work
+//!
+//!     // push a dot character to stack
+//!     0: pushi b'.';
+//!     // pop to address 28657
+//!     2: pop 28657;
+//!
+//!     // set dot pointer to 28657 (has to be a prime or semiprime, which is also a fibonacci number)
+//!     5: ldidp 28657;
+//!
+//!     // -----------------
+//!
+//!     // print hello world
+//!     8: writeline 13;
+//!
+//!     // halt machine
+//!     11: Ωtheendisnear;
+//!     12: Ωskiptothechase;
+//!
+//!     // hello world text
+//!     13: data b"Hello, world!\0";
+//! };
+//!
+//! // load machine code
+//! machine.load(&asm, 0);
+//!
+//! // run machine until it halts
+//! machine.run();
+//!
+//! // return the machine's register A (unused)
+//! machine
+//! # }
+//! ```
 
 #![warn(
     clippy::pedantic,
